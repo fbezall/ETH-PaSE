415ae82044e98eabee3e00702a24337e27e67608
fbezall <fbezall@gmail.com>
2020-08-01 15:40:42 -1200

    Holiday Assignment

---
+++ script.js
@@ -0,0 +1,16 @@
+alert("This is Fiifi's Assignment.");
+
+document.getElementById("para1").innerHTML = "<h1>Holiday Assignment</h1>";
+
+var a = 12 + 6 / 2 * 10;
+document.getElementById("para2").innerHTML = a;
+
+console.log(35 / 7 * 3);
+
+var X = 15;
+var Y = 10;
+var Z = X + Y;
+document.getElementById("para3").innerHTML = Z;
+
+
+
